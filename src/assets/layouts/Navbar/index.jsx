caa384491060751c2c2ff201ca1c9c35e71f0dae
MisterCxmpy <cosgunhussein@gmail.com>
2023-07-29 18:31:57 +0100

    style navbar

--- src/assets/layouts/Navbar/index.jsx
+++ src/assets/layouts/Navbar/index.jsx
@@ -1,13 +1,30 @@
 import React from 'react'
-import { Outlet } from 'react-router-dom'
+import {NavLink, Outlet } from 'react-router-dom'
+import styles from "./index.module.css"
 
 export default function Navbar() {
   return (
     <>
       <div className={styles['navbar']}>
-        
+        <NavLink to={"/"}><h1 className={styles['title']}>Trackify</h1></NavLink>
+        <div className={styles['navlinks']}>
+          <Link classes={["nav-link"]} to={"/"} content={"Showcase"} />
+          <Link classes={["nav-link"]} to={"/"} content={"About"} />
+          <Link classes={["nav-link"]} to={"/"} content={"Contact"} />
+        </div>
+        <Link classes={["sign-in-btn", "btn"]} to={"/"} content={"sign in"} />
       </div>
       <Outlet />
     </>
+  )
+}
+
+function Link({classes, to, content}) {
+  const combinedClasses = classes.map(className => styles[className]).join(" ");
+
+  return (
+    <NavLink to={to}>
+      <span className={combinedClasses}>{content}</span>
+    </NavLink>
   )
 }
