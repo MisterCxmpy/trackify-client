20ec500edfc63ea6c12c16c121631cac964eb9c9
MisterCxmpy <cosgunhussein@gmail.com>
2023-07-29 17:41:13 +0100

    setup routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
 
 function App() {
 
   return (
-    <div>
-
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<h1>Hello</h1>}>
+          
+        </Route>
+      </Routes>
+    </>
   )
 }
 
